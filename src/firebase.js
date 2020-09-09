4108d1d246d90609ed40d2148ae548a4ab27c7fb
Upshur06 <mupshur2005@gmail.com>
2020-09-09 02:45:50 -0400

    header_building

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCgxxljEns99ZN2Mcvqs3qaK2YpfWfM6Uo",
+    authDomain: "challenge-8287f.firebaseapp.com",
+    databaseURL: "https://challenge-8287f.firebaseio.com",
+    projectId: "challenge-8287f",
+    storageBucket: "challenge-8287f.appspot.com",
+    messagingSenderId: "1061025710640",
+    appId: "1:1061025710640:web:5d78542aa0d6891a7363f5",
+    measurementId: "G-41ESZ0RQNT"
+  };
