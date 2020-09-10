643442f4e8ca2ad87c23a64985431eeda0b1a8d7
Upshur06 <mupshur2005@gmail.com>
2020-09-10 02:09:26 -0400

    sub counter & price

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
-  return <div className="subtotal">
+  const [{ basket }, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
-              <strong>0</strong>
+              Subtotal ({basket.length} items):
+              <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -17,13 +22,15 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
-        displayType={"type"}
+        value={getBasketTotal(basket)} // Part of the homework
+        displayType={"text"}
+        thousandSeparator={true}
         prefix={"$"}
       />
 
       <button>Proceed to Checkout</button>
     </div>
+  );
 }
 
 export default Subtotal;
--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,10 @@ export const initialState = {
   basket: [],
 };
 
+//selector
+export const getBasketTotal = (basket) =>
+    basket?.reduce((amount, item) => amount + item.price, 0);
+
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
