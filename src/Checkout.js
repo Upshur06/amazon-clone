0c4571dbb5727c86058eecb5ba24f85cd08c04b3
Upshur06 <mupshur2005@gmail.com>
2020-09-09 22:12:12 -0400

    checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   return (
@@ -22,7 +23,7 @@ function Checkout() {
       </div>
       
       <div className="checkout__right">
-
+            <Subtotal />
       </div>
     </div>
   );
