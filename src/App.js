a5260f5e672ae696b93cd69f46fb3e2e62b0bae1
Upshur06 <mupshur2005@gmail.com>
2020-09-12 02:14:46 -0400

    checkout functioning

--- src/App.js
+++ src/App.js
@@ -5,35 +5,39 @@ import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
 import { auth } from "./firebase";
-import { useStateValue } from "./StateProvider"; 
+import { useStateValue } from "./StateProvider";
 
 function App() {
-  const [{},dispatch] = useStateValue();
+  const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
 
-    auth.onAuthStateChanged(authUser => {
-      console.log('THE USER IS >>> ', authUser)
-
-      if(authUser){
+      if (authUser) {
         dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
-      }else{
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
         dispatch({
-          type: 'SET_USER',
-          user: null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/payment">
+            <Header />
+            <Payment />
+          </Route>
           <Route path="/login">
             <Login />
           </Route>
---
+++ src/Payment.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
+
+function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length}</Link>)
+        </h1>
+        {/* Payment section - delivery address */}
+        <div className="payment__section">
+          <div clasName="payment__title">
+            <h3>Delevery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+
+        {/* Payment section - Review Items */}
+        <div className="payment__section">
+          <div clasName="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Payment section - Payment method */}
+        <div className="payment__section">
+          <div clasName="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+              {/* Strip Magic will go */}
+              </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
